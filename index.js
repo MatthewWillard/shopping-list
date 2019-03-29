abbd80e2e791f048d0a82db7f68967821eaf5ac5
Matthew Willard <MWillard85@gmail.com>
2019-03-29 14:00:21 -0600

    finished add item

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+$(function (){ 
+  $('#js-shopping-list-form').submit( function(event) {
+      const food = $('#shopping-list-entry').val();
+      $('.shopping-list').append
+          ('<li>' 
+              +'<span class="shopping-item">' 
+              + food 
+              + '</span>' 
+              + '<button class="shopping-item-toggle">' 
+              + '<span class="button-label">' 
+              + 'check' 
+              + '</span>' 
+              + '</button>' 
+              + ' '
+              + '<button class="shopping-item-delete">'
+              + '<span class="button-label">' 
+              + 'delete'
+              + '</span>'
+              + '</button>'
+           + '</li>');
+      event.preventDefault();
+  });
+})
+
+
 $(function() {          
   $(".shopping-list").on('click', '.shopping-item-toggle', function(event) {
     $(this).closest('li').find('.shopping-item').toggleClass("shopping-item__checked");  
